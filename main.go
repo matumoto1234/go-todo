9e5147b659a336c52cec2b904c97d2dd26201ed6
matumoto1234 <matumotosizuoka@gmail.com>
2022-03-27 17:39:08 +0900

    nice ping pong

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World! I am matumoto!!!!!!!!!!!!!!")
 }
 
+func ping_handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "pong")
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+  http.HandleFunc("/ping", ping_handler)
 	http.ListenAndServe(":8080", nil)
 }
