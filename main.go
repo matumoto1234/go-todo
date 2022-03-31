525fed38706e82a06f6cd9037a37aa1d046dc870
matumoto1234 <matumotosizuoka@gmail.com>
2022-03-31 21:56:35 +0900

    toriaezu commit

--- main.go
+++ main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
-)
+	"net/http"
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World! I am matumoto!!!!!!!!!!!!!!")
-}
+	"github.com/matumoto1234/go-todo/chat"
+)
 
 func ping_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	handler := chat.TemplateHandler{Filename: "chat.html"}
+	http.Handle("/", &handler)
+
 	http.HandleFunc("/ping", ping_handler)
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
